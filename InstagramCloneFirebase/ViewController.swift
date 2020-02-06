0b5826c0d2bd368056be87ca57eab8a8a4e6f15f
Cem Ayaz <cem.ayaz@tubitak.gov.tr>
2020-02-06 11:13:17 +0300

    Initial Commit

---
+++ InstagramCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by cemayaz on 6.02.2020.
+//  Copyright © 2020 cemayaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
