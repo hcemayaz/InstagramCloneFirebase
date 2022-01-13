92323125d674818fadbed23c2f7d4e524171dd76
cem ayaz <cem.ayaz@tubitak.gov.tr>
2022-01-14 01:32:26 +0300

    Commit Applied

---
+++ InstagramCloneFirebase/FeedCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCellTableViewCell.swift
+//  InstagramCloneFirebase
+//
+//  Created by cemayaz on 11.02.2020.
+//  Copyright © 2020 cemayaz. All rights reserved.
+//
+
+import UIKit
+
+class FeedCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ InstagramCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by cemayaz on 6.02.2020.
+//  Copyright © 2020 cemayaz. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        }catch{
+            print("Error")
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
